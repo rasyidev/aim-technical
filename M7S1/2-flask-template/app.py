ff3851dc6a209188a82ddf06ad3f5ee4f121116b
rasyidev <habib.rasyid11@gmail.com>
2022-04-05 13:19:09 +0700

    Update view for 2-flask-template

--- M7S1/2-flask-template/app.py
+++ M7S1/2-flask-template/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/", )
-def welcome_page():
-    return render_template("welcome_page.html")
+@app.route("/", methods=['GET', 'POST'])
+def iris_prediction():
+    if request.method == 'GET':
+        return render_template("iris-prediction.html")
+    else:
+        print(request)
+        return jsonify(request.args)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
